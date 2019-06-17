d1ceb69f62768da72ec5b37e036b2af8da81deb8
tangdada1209 <1906618342@qq.com>
2019-06-17 11:50:33 +0800

    111111

--- src/main/java/cn/huawe/staycuteblog/controller/UserContrroller.java
+++ src/main/java/cn/huawe/staycuteblog/controller/UserContrroller.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserContrroller {
-    //牛逼
+    //牛逼kkkkkkls
     @RequestMapping("/hello")
     @ResponseBody
 	public String hello(){
