f58801fbaafe56aeb9ef6c8d3c7c440bb211e06c
tangdada1209 <1906618342@qq.com>
2019-06-26 08:39:42 +0800

    baobaobao

--- src/main/resources/static/default/js/yido.js
+++ src/main/resources/static/default/js/yido.js
@@ -1,32 +1,35 @@
+$(function(){
 
-        $('.navRight li').mouseenter(function(){
+    $('.navRight li').mouseenter(function(){
 
-            $(this).children('span').css('transform','rotate(135deg)');
+        $(this).children('span').css('transform','rotate(135deg)');
 
-        }).mouseleave(function(){
+    }).mouseleave(function(){
 
-            $(this).children('span').css('transform','rotate(-45deg)');
+        $(this).children('span').css('transform','rotate(-45deg)');
 
-        })
+    })
 
-        $('.fenlei ul li').mouseenter(function(){
+    $('.fenlei ul li').mouseenter(function(){
 
-            $(this).stop().animate({'height':'289px'},300).siblings().stop().animate({'height':'44px'},300);
+        $(this).stop().animate({'height':'289px'},300).siblings().stop().animate({'height':'44px'},300);
 
-            $(this).siblings().css('background','#E8E5DD');
+        $(this).siblings().css('background','#E8E5DD');
 
-            $('.fenleiright').fadeTo(0,0.8).stop().animate({'width':'289px'},300);
+        $('.fenleiright').fadeTo(0,0.8).stop().animate({'width':'289px'},300);
 
-        }).mouseleave(function(){
+    }).mouseleave(function(){
 
-            $('.fenlei ul li').stop().animate({'height':'79px'},300)
+        $('.fenlei ul li').stop().animate({'height':'79px'},300)
 
-            $(this).siblings().css('background','#E8E5DD');
+        $(this).siblings().css('background','#E8E5DD');
 
-        });
+    });
 
-        $('.navLeft').mouseleave(function(){
+    $('.navLeft').mouseleave(function(){
 
-            $('.fenleiright').stop().animate({'width':'0px'},300);
+        $('.fenleiright').stop().animate({'width':'0px'},300);
 
-        });
+    });
+
+});
